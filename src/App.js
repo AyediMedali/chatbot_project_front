a7a398074cd05c61a1f15e556e26781b5830c6a1
Medali <medali.ayedi@esprit.tn>
2019-04-03 13:30:07 +0100

    Revert "Template"
    This reverts commit 8fb564d29cf26152805be7dbe540b5adf74f5ca9.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './containers/Header';
-import Footer from './containers/Footer';
-import Home from './containers/Home' ;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <body>
-        <Header/>
-        <Home/>
-        <Footer/>
-        </body>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
