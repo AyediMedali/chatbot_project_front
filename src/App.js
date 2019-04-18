00f762483f6075b003c8cf2f65a9ef3d2ed836de
haddadMoez <moez.haddad@esprit.tn>
2019-04-19 00:11:36 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import socketIOClient from "socket.io-client";
 import {BrowserRouter, Route, Switch} from "react-router-dom" ; 
 
@@ -10,40 +10,6 @@ import About from './components/About';
 
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {messages : []}
-  }
-
-  componentDidMount() {
-    this.socket = socketIOClient('http://localhost:3000/');
-    this.socket.on('ai response', function(response) {
-    console.log(response)
-
-      const msg = {
-        body: response,
-        from: 'robot'
-      }
-     // console.log(msg)
-
-      //this.setState({messages:[msg, ...this.state.messages]})
-
-    })
-  }
-
-  handleSubmit = event =>{
-    const body  = event.target.value
-    if(event.keyCode === 13  && body){
-      const message = {
-        body,
-        from: 'Me'
-      }
-    //  console.log(body + " msgg")
-      this.setState({messages:[message, ...this.state.messages]})
-      this.socket.emit('chat request',body)
-      event.target.value = ''
-    }
-  }
 
 
  /* render() {
@@ -68,7 +34,7 @@ render() {
 
     <BrowserRouter>
       <div>
-        <Navbar/>
+        {/*<Navbar/>*/}
         <Switch>
           <Route path='/' component={Home} exact></Route>
           <Route path='/about' component={About}></Route>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,70 +1,59 @@
 import React, {Component} from 'react';
+import socketIOClient from "socket.io-client";
 
 class Home extends Component {
-    render() {
-        return(
-            <div id="main" className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <h1>Bug_Hunters Chatbot</h1>
-                <h2 />
-                <div className="b_integrations_agent_links">
-                  <a href="#" data-toggle="modal" data-target="#embedModal"><i className="fa-code fa" />Embed</a>
-                  <a href="mailto:?body=Hey%2C%20check%20out%20my%20bot%20that%20I%20built%20with%20Dialogflow%3A%20https%3A%2F%2Fbot.dialogflow.com%2F175d2574-d9f2-4736-94b0-05f182e5b595%0A%0ACreate%20yours%20at%20https%3A%2F%2Fdialogflow.com%2F&subject=Bot%20PI_Chatbot%20on%20Dialogflow"><i className="fa-envelope-o fa" target="_blank" />Email</a>
-                  <a href="#" data-toggle="modal" data-target="#shareModal"><i className="fa-reply fa" />Share</a>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <iframe id="frame" className="b_frame" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/175d2574-d9f2-4736-94b0-05f182e5b595" />
-              </div>
-            </div>
-            <div className="modal fade" tabIndex={-1} id="embedModal" role="dialog">
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 className="modal-title">Use following code to integrate this agent into your site:</h4>
-                  </div>
-                  <div className="modal-body">
-                    <pre>&lt;iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/175d2574-d9f2-4736-94b0-05f182e5b595"&gt;&lt;/iframe&gt;</pre>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="modal fade" tabIndex={-1} id="shareModal" role="dialog">
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 className="modal-title">Share this agent via social networks:</h4>
-                  </div>
-                  <div className="modal-body">
-                    <a href="https://www.facebook.com/sharer.php?u=https://bot.dialogflow.com/175d2574-d9f2-4736-94b0-05f182e5b595" target="_blank">
-                      <img src="https://assets.dialogflow.com/common/assets/img/social_share/facebook.png" alt="Facebook" />
-                    </a>
-                    {/* Twitter */}
-                    <a href="https://twitter.com/share?text=PI_Chatbot%20https://bot.dialogflow.com/175d2574-d9f2-4736-94b0-05f182e5b595%20at%20@dialogflow.com&url=false" target="_blank">
-                      <img src="https://assets.dialogflow.com/common/assets/img/social_share/twitter.png" alt="Twitter" />
-                    </a>
-                    {/* Google+ */}
-                    <a href="https://plus.google.com/share?url=https://bot.dialogflow.com/175d2574-d9f2-4736-94b0-05f182e5b595" target="_blank">
-                      <img src="https://assets.dialogflow.com/common/assets/img/social_share/google.png" alt="Google" />
-                    </a>
-                    {/* LinkedIn */}
-                    <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://bot.dialogflow.com/175d2574-d9f2-4736-94b0-05f182e5b595" target="_blank">
-                      <img src="https://assets.dialogflow.com/common/assets/img/social_share/linkedin.png" alt="LinkedIn" />
-                    </a>
-                    {/* Reddit */}
-                    <a href="https://reddit.com/submit?url=https://bot.dialogflow.com/175d2574-d9f2-4736-94b0-05f182e5b595&title=PI_Chatbot.%20" target="_blank">
-                      <img src="https://assets.dialogflow.com/common/assets/img/social_share/reddit.png" alt="Reddit" />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
+  constructor(props){
+    super(props)
+    this.state = {messages : []}
+  }
+  componentDidMount() {
+    this.socket = socketIOClient('http://localhost:3000/');
+    this.socket.on('ai response', function(response) {
+      console.log(response)
+
+      const msg = {
+        body: response,
+        from: 'robot'
+      }
+      console.log(msg)
+      // this.state = {messages : []}
+      console.log(this.state)
+      //  console.log(this.state.messages)
+
+
+      this.setState({messages:[...this.state.messages, msg ]})
+      console.log(this.state.messages)
+
+    }.bind(this))
+  }
+
+  handleSubmit = event =>{
+    const body  = event.target.value
+    if(event.keyCode === 13  && body){
+      const message = {
+        body,
+        from: 'Me'
+      }
+      console.log(this.state)
+
+      this.setState({messages:[...this.state.messages, message ]})
+      this.socket.emit('chat request',body)
+      event.target.value = ''
     }
+  }
+
+
+  render() {
+    const messages = this.state.messages.map((message, index)=> {
+      return <li key={index}><b>{message.from}:</b>{message.body}</li>
+    })
+    return (
+        <div id="main" className="container">
+          <input type="text" placeholder='enter a message' onKeyUp={this.handleSubmit}/>
+          {messages}
+        </div>
+    );
+  }
 }
 
 export default Home;
