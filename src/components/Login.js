f905c85f9d260cf2689d9ff000f6cad4d1376c84
haddadMoez <moez.haddad@esprit.tn>
2019-04-19 00:23:48 +0100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Button, FormGroup, FormControl, FormLabel} from "react-bootstrap";
 import "./Login.css";
 
 class Login extends React.Component {
@@ -52,33 +53,33 @@ class Login extends React.Component {
     render() {
         return (
             <div className="Login">
-                {/*<form onSubmit={this.handleSubmit}>*/}
-                    {/*<FormGroup controlId="email" bsSize="large">*/}
-                        {/*<FormLabel>Email</FormLabel>*/}
-                        {/*<FormControl*/}
-                            {/*autoFocus*/}
-                            {/*type="email"*/}
-                            {/*value={this.state.email}*/}
-                            {/*onChange={this.handleChange}*/}
-                        {/*/>*/}
-                    {/*</FormGroup>*/}
-                    {/*<FormGroup controlId="password" bsSize="large">*/}
-                        {/*<FormLabel>Password</FormLabel>*/}
-                        {/*<FormControl*/}
-                            {/*value={this.state.password}*/}
-                            {/*onChange={this.handleChange}*/}
-                            {/*type="password"*/}
-                        {/*/>*/}
-                    {/*</FormGroup>*/}
-                    {/*<Button*/}
-                        {/*block*/}
-                        {/*bsSize="large"*/}
-                        {/*disabled={!this.validateForm()}*/}
-                        {/*type="submit"*/}
-                    {/*>*/}
-                        {/*Login*/}
-                    {/*</Button>*/}
-                {/*</form>*/}
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="email" bsSize="large">
+                        <FormLabel>Email</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bsSize="large">
+                        <FormLabel>Password</FormLabel>
+                        <FormControl
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        bsSize="large"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </form>
             </div>
         );
     }
