7f7373ddd44b457484c29ff6bc763f8991f61057
haddadMoez <moez.haddad@esprit.tn>
2019-04-21 20:40:20 +0100

    fix login/logout button

--- src/App.js
+++ src/App.js
@@ -16,77 +16,12 @@ import storage from 'node-sessionstorage'
 class App extends Component {
 
 
-    constructor(props) {
-        super(props);
-        console.log(localStorage.getItem('user') + " constructor")
-        console.log(localStorage.getItem('token') + " token constructor")
-        this.state = {isLoggedIn: false};
-    }
-
-    componentDidMount() {
-        if(localStorage.getItem('user') == '' || localStorage.getItem('user') == null)
-        {
-            this.setState({isLoggedIn:false})
-        }
-        else {
-            this.setState({isLoggedIn:true})
-        }
-    }
 
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        const divStyle = {
-            top: '190px',
 
-        };
-        let button;
-        if (isLoggedIn) {
-            button =
-                <div style={divStyle}
-                    className='fxb-col fxb fxb-end-x fxb-center-y fxb-basis-auto fxb-sm-half site-header-col-right site-header-main-right'>
-                    <div className="fxb fxb-row fxb-row-col fxb-center-y">
-
-                        <div
-                            className='fxb-col fxb fxb-end-x fxb-center-y fxb-basis-auto fxb-sm-half site-header-main-right-top'>
-                            <a href="/logout" id="ctabutton"
-                               className="sh-component ctabutton kl-cta-ribbon " target="_self"><strong>LOGOUT</strong>INTRANET
-
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-
-            {/*<button >*/}
-                {/*<a href="/logout">Logout </a>*/}
-            {/*</button>*/}
-        } else {
-            button =
-                <div
-                    className='fxb-col fxb fxb-end-x fxb-center-y fxb-basis-auto fxb-sm-half site-header-col-right site-header-main-right'>
-                    <div className="fxb fxb-row fxb-row-col fxb-center-y">
-
-                        <div
-                            className='fxb-col fxb fxb-end-x fxb-center-y fxb-basis-auto fxb-sm-half site-header-main-right-top'>
-                            <a href="/login" id="ctabutton"
-                               className="sh-component ctabutton kl-cta-ribbon " target="_self"><strong>LOGIN</strong>INTRANET
-
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-
-            {/*<button>*/}
-                {/*<a href="/login">Login </a>*/}
-            {/*</button>*/}
-        }
         return (
             <React.Fragment>
-
-                    {button}
-
 
                     <BrowserRouter>
                     
