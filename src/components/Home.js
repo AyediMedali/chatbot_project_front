6e4c1afe6345a6481cb22c0768d390a0abca59be
haddadMoez <moez.haddad@esprit.tn>
2019-04-26 16:24:14 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -63,10 +63,10 @@ class Home extends Component {
                         if (this.state.page == 'SkillsPage') {
                             this.setState({skills: res.user.skills})
                         } else if (this.state.page == 'PfePage') {
+                            console.log(res.user.internships)
                             this.setState({best_pfe: res.user.internships})
                         }
                         if (this.state.page == 'ExamPage') {
-                            console.log(res.user.exams)
                             this.setState({exams: res.user.exams})
                         }
 
@@ -225,7 +225,7 @@ class Home extends Component {
                                                                 {/*<h4 className="text action_box-title">COMPUTER SCIENCE AND COMMUNICATION: 1st and 2nd Cycle</h4>*/}
                                                                 {/*<h5 className="ac-subtitle action_box-subtitle">information and communication technologies </h5>*/}
                                                                 <ul >
-                                                                    <li className="text action_box-title" > Subject: 
+                                                                    <li className="text action_box-title" > Subject:
                                                                         {item.exam_subject.name}
                                                                     </li>
                                                                     <li className="ac-subtitle action_box-subtitle" > Exam type : {item.exam_type} </li>
@@ -245,61 +245,12 @@ class Home extends Component {
                                             </div>
                                         </div>
                                     </section>
-                                    {/*<form action="">*/}
-                                        {/*<fieldset style={fieldsetStyle} >*/}
-                                            {/*<legend style={legendStyle}>********* EXAM ********</legend>*/}
-                                            {/*<ul style={{'margin':'10px'}}>*/}
-                                                {/*<li > Exam type : {item.exam_type} </li>*/}
-                                                {/*<li> Coef : {item.coef}</li>*/}
-                                                {/*<li> Passing date : {item.passing_date} </li>*/}
-                                                {/*<li> Passing way : {item.passing_way} </li>*/}
-                                                {/*<li> Old exmas : {item.url_drive} </li>*/}
-                                            {/*</ul>*/}
-
-                                        {/*</fieldset>*/}
-                                    {/*</form>*/}
 
                                 </div>
 
                             ))}
 
                         </div>
-
-
-                    {/*<div className='container'>*/}
-                        {/*<div className="row" >*/}
-                            {/*{items.map(item => (*/}
-
-
-                                {/*<div className="col-sm-8"  key={item._id}>*/}
-                                    {/*<br/>*/}
-
-
-                                    {/*<form action="">*/}
-                                        {/*<fieldset style={fieldsetStyle} >*/}
-                                            {/*<legend style={legendStyle}>********* EXAM ********</legend>*/}
-                                            {/*<ul style={{'margin':'10px'}}>*/}
-                                                {/*<li > Exam type : {item.exam_type} </li>*/}
-                                                {/*<li> Coef : {item.coef}</li>*/}
-                                                {/*<li> Passing date : {item.passing_date} </li>*/}
-                                                {/*<li> Passing way : {item.passing_way} </li>*/}
-                                                {/*<li> Old exmas : {item.url_drive} </li>*/}
-                                            {/*</ul>*/}
-
-                                        {/*</fieldset>*/}
-                                    {/*</form>*/}
-
-                                {/*</div>*/}
-
-                            {/*))}*/}
-
-                        {/*</div>*/}
-                    {/*</div>*/}
-
-
-
-
-
                     </div>
 
                 );
@@ -326,13 +277,57 @@ class Home extends Component {
 
                                 <div className="col-sm-8" key={item._id}>
 
-                                    <br/>********* PFE ******** <br/>
-                                    Entreprise : {item.entreprise} <br/>
-                                    Description : {item.description} <br/>
-                                    Profile request : {item.profile_request} <br/>
-                                    Start date : {item.start_date} <br/>
-                                    End date : {item.end_date} <br/>
-                                    Location : {item.location} <br/>
+
+
+                                    <br/>
+
+                                    <section className="   section--no " id="eluid1e6a7716">
+                                        <div className="zn_section_size container">
+                                            <div className="row zn_columns_container zn_content " data-droplevel={1}>
+                                                <div className="eluid00a9d24a   col-md-8 col-sm-8   zn_sortable_content zn_content " data-droplevel={2}>
+                                                    <div className="action_box eluide80e654a  actionbox--light style1" data-arrowpos="center">
+                                                        <div className="action_box_inner action_box-inner">
+                                                            <div className="action_box_content action_box-content">
+                                                                <div className="ac-content-text action_box-text">
+                                                                    {/*<h4 className="text action_box-title">COMPUTER SCIENCE AND COMMUNICATION: 1st and 2nd Cycle</h4>*/}
+                                                                    {/*<h5 className="ac-subtitle action_box-subtitle">information and communication technologies </h5>*/}
+                                                                    <ul >
+                                                                        <li className="text action_box-title" > Entreprise:
+                                                                            {item.entreprise}
+                                                                        </li>
+                                                                        <li className="ac-subtitle action_box-subtitle" > Description: {item.description} </li>
+                                                                        <li className="ac-subtitle action_box-subtitle" > Skills: {item.skills[0].title} </li>
+                                                                        <li className="ac-subtitle action_box-subtitle"> Profile request : {item.profile_request}</li>
+                                                                        <li className="ac-subtitle action_box-subtitle"> Start date : {item.start_date} </li>
+                                                                        <li className="ac-subtitle action_box-subtitle"> End date : {item.end_date} </li>
+                                                                        <li className="ac-subtitle action_box-subtitle"> Location : {item.location} </li>
+                                                                    </ul>
+                                                                </div>
+                                                                <div className="ac-buttons action_box-buttons"><a href="http://esprit.tn/?page_id=2370" className="btn ac-btn action_box-button action_box-button-first btn-lined btn-skewed" target="_self">SHOW MORE
+                                                                </a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+
+
+
+                                    {/*<br/>********* PFE ******** <br/>*/}
+                                    {/*Entreprise : {item.entreprise} <br/>*/}
+                                    {/*Description : {item.description} <br/>*/}
+                                    {/*Profile request : {item.profile_request} <br/>*/}
+                                    {/*Start date : {item.start_date} <br/>*/}
+                                    {/*End date : {item.end_date} <br/>*/}
+                                    {/*Location : {item.location} <br/>*/}
+
+
+
+
+
 
                                 </div>
 
